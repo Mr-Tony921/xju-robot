d4b3e0d3195ff4c7a9c2e3a3a6bbeacaa6bb628e
Mr.Tony <tongronglei@163.com>
2022-10-12 17:17:59 +0800

    Add move base flex launch and exe path service

--- src/pnc/include/xju_pnc/state_machine.h
+++ src/pnc/include/xju_pnc/state_machine.h
@@ -12,15 +12,23 @@
 #include <tf2_ros/transform_listener.h>
 #include <vector>
 
+#include "mbf_msgs/MoveBaseAction.h"
+#include "mbf_msgs/ExePathAction.h"
+#include "xju_pnc/exe_path.h"
 #include "xju_pnc/record_start.h"
 #include "xju_pnc/record_stop.h"
 
 namespace xju::pnc {
 constexpr static const char* NODE_NAME = "xju_state_machine";
+constexpr static const char* DEFAULT_DIR = "/home/tony/course_ws/path";
 constexpr static const char* RECORD_START_SRV = "xju_record_start";
 constexpr static const char* RECORD_STOP_SRV = "xju_record_stop";
+constexpr static const char* EXE_PATH_SRV = "xju_task";
 constexpr static const double RECORD_PATH_LEN_DENS = 0.05;
 constexpr static const double RECORD_PATH_AGU_DENS = 10 * M_PI / 180;
+
+using GotoCtrl = actionlib::SimpleActionClient<mbf_msgs::MoveBaseAction>;
+using ExeCtrl = actionlib::SimpleActionClient<mbf_msgs::ExePathAction>;
 
 class StateMachine {
 public:
@@ -39,6 +47,16 @@ private:
 
   auto robot_pose() -> std::optional<geometry_msgs::Pose>;
 
+  auto send_goto(size_t const& index) -> bool;
+
+  void goto_done(const actionlib::SimpleClientGoalState& state, const mbf_msgs::MoveBaseResultConstPtr& result);
+
+  auto send_exe(size_t const& index) -> bool;
+
+  void exe_done(const actionlib::SimpleClientGoalState& state, const mbf_msgs::ExePathResultConstPtr& result);
+
+  auto exe_path_service(xju_pnc::exe_path::Request& req, xju_pnc::exe_path::Response& resp) -> bool;
+
   auto record_start_service(xju_pnc::record_start::Request& req, xju_pnc::record_start::Response& resp) -> bool;
 
   auto record_stop_service(xju_pnc::record_stop::Request& req, xju_pnc::record_stop::Response& resp) -> bool;
@@ -51,6 +69,8 @@ private:
 
   auto write_file(std::string& file_path) -> bool;
 
+  auto read_file(std::string& file_path) -> bool;
+
 private:
   std::unique_ptr<tf2_ros::Buffer> tf_;
   std::unique_ptr<tf2_ros::TransformListener> tfl_;
@@ -59,10 +79,19 @@ private:
   ros::Publisher record_path_pub_;
   ros::ServiceServer record_start_srv_;
   ros::ServiceServer record_stop_srv_;
+  ros::ServiceServer exe_path_srv_;
+
+  std::unique_ptr<GotoCtrl> goto_ctrl_;
+  std::unique_ptr<ExeCtrl> exe_ctrl_;
 
   std::string file_path_;
   std::atomic_bool recording_;
+  std::atomic_bool running_;
+  std::atomic_bool pause_;
 
   std::vector<geometry_msgs::Pose> record_path_;
+  nav_msgs::Path cur_route_;
+
+  size_t current_index_;
 };
 }
