4eae84f6ee88fd8b8a019171bb7bc1061ece97f1
Mr.Tony <tongronglei@163.com>
2023-04-12 13:41:28 +0800

    Add tricycle model

--- src/simu/script/servo_commands.py
+++ src/simu/script/servo_commands.py
@@ -13,17 +13,21 @@ def set_throttle_steer(data):
 
     pub_vel_left_rear_wheel = rospy.Publisher('/xju/left_rear_wheel_velocity_controller/command', Float64, queue_size=1)
     pub_vel_right_rear_wheel = rospy.Publisher('/xju/right_rear_wheel_velocity_controller/command', Float64, queue_size=1)
+    pub_vel_front_wheel = rospy.Publisher('/xju/front_wheel_velocity_controller/command', Float64, queue_size=1)
 
     pub_pos_left_steering_hinge = rospy.Publisher('/xju/left_steering_hinge_position_controller/command', Float64, queue_size=1)
     pub_pos_right_steering_hinge = rospy.Publisher('/xju/right_steering_hinge_position_controller/command', Float64, queue_size=1)
+    pub_pos_front_steering_hinge = rospy.Publisher('/xju/front_steering_hinge_position_controller/command', Float64, queue_size=1)
 
     throttle = data.drive.speed*14.5
     steer = data.drive.steering_angle
 
     pub_vel_left_rear_wheel.publish(throttle)
     pub_vel_right_rear_wheel.publish(throttle)
+    pub_vel_front_wheel.publish(throttle)
     pub_pos_left_steering_hinge.publish(steer)
     pub_pos_right_steering_hinge.publish(steer)
+    pub_pos_front_steering_hinge.publish(steer)
 
 def servo_commands():
 
