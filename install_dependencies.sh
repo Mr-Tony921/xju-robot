8aabadd4302aba0ce279df1c785c24112b938eb0
Mr.Tony <tongronglei@163.com>
2023-05-16 14:08:01 +0800

    Add scripts for noetic dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install -y libsdl1.2-dev libsdl-image1.2-dev ros-noetic-octomap ros-noetic-octomap-ros ros-noetic-move-base-msgs ros-noetic-tf2-sensor-msgs ros-noetic-sbpl libsuitesparse-dev ros-noetic-libg2o
